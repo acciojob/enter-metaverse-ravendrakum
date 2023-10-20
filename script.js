a2911d7981f406079995bf5488f7f745acf77a98
Ravendra kumar <111950764+ravendrakum@users.noreply.github.com>
2023-10-20 19:07:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function changeText() {
+	 var status = document.getElementById("status");
+     var outputText = document.getElementById("outputText");
+            
+            // Update the content in the p tag
+            status.textContent = "Entered Metaverse";
+            
+            // Display the updated content in an h1 tag
+            outputText.textContent = status.textContent;
+}
